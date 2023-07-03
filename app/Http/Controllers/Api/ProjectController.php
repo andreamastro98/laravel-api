1dfa62a5ba39845775c92546a17843cdb711f57e
andreamastro98 <andreamastrovito98@gmail.com>
2023-07-03 12:43:21 +0200

    if condition technology added to the controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,24 +9,45 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(Request $request){
-	 //$projects = Project::all();
 
-//oppure per avere anche le relazioni
+		//$projects = Project::all();
 
-	//$projects = Project::with('type','technologies')->get();
+		//oppure per avere anche le relazioni
 
-//oppure per avere tot(3) elementi per pagina 
+		//$projects = Project::with('type','technologies')->get();
 
-	if ($request->has('type_id')) {
-		
-		$projects = Project::with('type', 'technologies')->where('type_id', $request->type_id)->paginate(3);
+		//oppure per avere tot(3) elementi per pagina 
 
-	} else {
+		// if ($request->has('type_id')) {
 
-		$projects = Project::with('type', 'technologies')->paginate(3);
+		// 	$projects = Project::with('type', 'technologies')->where('type_id', $request->type_id)->paginate(3);
 
-	}
+		// } else {
 
+		// 	$projects = Project::with('type', 'technologies')->paginate(3);
+
+		// }
+
+		$query = Project::with(['type', 'technologies']);
+
+		//prima condizione per filtrare solo i progetti che hanno un tipo uguale a quello della request
+
+		if ($request->has('type_id')) {
+			$query->where('type_id', $request->type_id);
+		}
+
+		//seconda condizione per filtrare solo i progetti che hanno una tecnologia uguale a quello della request
+
+		if ($request->has('technologies_ids')) {
+
+			$technologiesIds = explode(',', $request->technologies_ids);
+
+			$query->whereHas('technologies', function ($query) use ($technologiesIds) {
+				$query->whereIn('id', $technologiesIds);
+			});
+		}
+
+		$projects = $query->paginate(3);
 
 	return response()->json([
 		'success' => true,
---
+++ app/Http/Controllers/Api/TechnologyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $technologies = Technology::all();
+
+        return response()->json(
+            [
+                'success' => true,
+                'technologies' => $technologies
+            ]
+        );
+    }
+
+}
